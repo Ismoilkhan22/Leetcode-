e769f5ef7eb1ff0434bf6fd5d3dbf69ca060ea29
Ismoilkhan22 <ismoilkhan2205@gmail.com>
2024-07-13 14:21:57 +0500

    Romon to integer

--- algarethms/Easy.py
+++ algarethms/Easy.py
@@ -78,7 +78,75 @@ class Solution2:
 
     
 
+# 3-masala
+"""
+Rim raqamlari yetti xil belgi bilan ifodalanadi:  I, V, X, L, Cva D.M
+
+Belgi        qiymati
+men 1
+V 5
+X 10
+L 50
+C 100
+D 500
+M 1000
+Masalan,  Rim raqamida 2yozilganidek II , ikkitasi qo'shiladi. 12kabi yoziladi  XII, bu oddiy X + II. Raqam 27quyidagicha yoziladi XXVII, ya'ni XX + V + II.
+
+Rim raqamlari odatda chapdan o'ngga kattadan kichikga yoziladi. Biroq, to'rt uchun raqam emas IIII. Buning o'rniga to'rt raqami sifatida yoziladi IV. Biri beshdan oldin bo'lgani uchun, biz uni to'rtta qilib ayiramiz. Xuddi shu tamoyil to'qqiz soniga ham tegishli bo'lib, u quyidagicha yoziladi IX. Ayirish qo'llaniladigan oltita holat mavjud:
+
+IV4 va 9 ni hosil qilish uchun (5) va X(10)  dan oldin joylashtirilishi mumkin .
+X40 va 90 qilish uchun  L(50) va (100) dan oldin joylashtirilishi mumkin .C
+C400 va 900 qilish uchun D(500) va (1000) dan oldin joylashtirilishi mumkin .M
+Rim raqami berilgan bo'lsa, uni butun songa aylantiring.
+
+ 
+
+1-misol:
+
+Kirish: s = "III"
+ Chiqish: 3
+ Izoh: III = 3.
+2-misol:
+
+Kirish: s = "LVIII"
+ Chiqish: 58
+ Izoh: L = 50, V= 5, III = 3.
+3-misol:
+
+Kirish: s = "MCMXCIV"
+ Chiqish: 1994
+ Tushuntirish: M = 1000, CM = 900, XC = 90 va IV = 4.
+ 
+
+Cheklovlar:
+
+1 <= s.length <= 15
+sfaqat belgilarni o'z ichiga oladi ('I', 'V', 'X', 'L', 'C', 'D', 'M').
+Bus diapazonda haqiqiy rim raqami  ekanligi kafolatlanadi [1, 3999].
+
+"""
 
 
+# solutions :
 
-
+def romanToInt(self, s: str) -> int:
+    m = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+    
+    ans = 0
+    
+    for i in range(len(s)):
+        if i < len(s) - 1 and m[s[i]] < m[s[i+1]]:
+            ans -= m[s[i]]
+        else:
+            ans += m[s[i]]
+    
+    return ans
+    
