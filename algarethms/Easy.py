576c64e39d0182fc78d34bdd1fbe135e607d6918
Ismoilkhan22 <ismoilkhan2205@gmail.com>
2024-08-05 15:54:24 +0500

    easy 20

--- algarethms/Easy.py
+++ algarethms/Easy.py
@@ -25,14 +25,14 @@ Output: [0,1]
 """
 
 
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        n = len(nums)
-        for i in range(n-1):
-            for j in range(i+1, n):
-                if nums[i]+nums[j] == target:
-                    return [i, j]
-        return [] # No solution found
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         n = len(nums)
+#         for i in range(n-1):
+#             for j in range(i+1, n):
+#                 if nums[i]+nums[j] == target:
+#                     return [i, j]
+#         return [] # No solution found
     
 
 
@@ -62,19 +62,19 @@ Input: x = 10
 Output: false
 Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
  
-"""
-class Solution2:
-    def isPalindrome(self, x: int ) ->bool:
-        if  x<0:
-            return False
-        reversed_num = 0
-        temp  = x
+# """
+# class Solution2:
+#     def isPalindrome(self, x: int ) ->bool:
+#         if  x<0:
+#             return False
+#         reversed_num = 0
+#         temp  = x
 
-        while temp !=0:
-            digit = temp % 10
-            reversed_num = reversed_num * 10 + digit
-            temp // 10
-        return reversed_num == x 
+#         while temp !=0:
+#             digit = temp % 10
+#             reversed_num = reversed_num * 10 + digit
+#             temp // 10
+#         return reversed_num == x 
 
     
 
@@ -129,24 +129,25 @@ Bus diapazonda haqiqiy rim raqami  ekanligi kafolatlanadi [1, 3999].
 
 # solutions :
 
-def romanToInt(self, s: str) -> int:
-    m = {
-        'I': 1,
-        'V': 5,
-        'X': 10,
-        'L': 50,
-        'C': 100,
-        'D': 500,
-        'M': 1000
-    }
-    
-    ans = 0
-    
-    for i in range(len(s)):
-        if i < len(s) - 1 and m[s[i]] < m[s[i+1]]:
-            ans -= m[s[i]]
-        else:
-            ans += m[s[i]]
-    
-    return ans
-    
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        m = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        
+        ans = 0
+        
+        for i in range(len(s)):
+            if i < len(s) - 1 and m[s[i]] < m[s[i+1]]:
+                ans -= m[s[i]]
+            else:
+                ans += m[s[i]]
+        
+        return ans
+
