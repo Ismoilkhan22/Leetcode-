884bc19670fd7fc082e1c5028cbb91752bd52e1b
Ismoilkhan22 <ismoilkhan2205@gmail.com>
2024-10-14 22:50:08 +0500

    26 next yechim

--- algarethms/easy.py
+++ algarethms/easy.py
@@ -39,17 +39,14 @@
 
 #  solution 
 class Solution:
-    def removeDuplicates(self, nums: List[int]) -> int:
-        i = 0
-
-        for j in range(1, len(nums)):
-            if nums[j] != nums[i]:
-                i += 1
-                nums[i] = nums[j]
-        
-        return i + 1
-
-
+	def removeDuplicates(self, nums: List[int]) -> int:
+		i = 1
+		while i < len(nums):
+			if nums[i] == nums[i - 1]:
+				nums.pop(i)
+			else:
+				i += 1
+		return len(nums)
 
 
 
