0e5b15573c04743e22b3d1f7248b048211278772
Ismoilkhan22 <ismoilkhan2205@gmail.com>
2024-12-26 13:03:58 +0500

    isAnagram

--- DSA/day_one.py
+++ DSA/day_one.py
@@ -58,7 +58,79 @@ from typing import List
 
 # solutions4  shakaladni yechim.
 
-def hasDublicate(nums: List[int]) -> bool:
-    return len(set(nums))< len(nums)
+# def hasDublicate(nums: List[int]) -> bool:
+#     return len(set(nums))< len(nums)
 
-print(hasDublicate([1,2,3,3]))
+# print(hasDublicate([1,2,3,3]))
+
+
+
+
+"""
+    problem 2    
+"""
+
+"""
+Given two strings s and t, return true if the two strings are anagrams of each other,
+otherwise return false.
+
+An anagram is a string that contains the exact same characters as another string, 
+but the order of the characters can be different.
+Example 1:
+
+Input: s = "racecar", t = "carrace"
+
+Output: true
+
+Example 2:
+
+Input: s = "jar", t = "jam"
+
+Output: false
+
+
+"""
+# solution1
+
+
+# def isAnagram(s: str, t: str) -> bool:
+#     return sorted(s) == sorted(t)
+
+
+# solution2
+
+# def isAnagram(s: str, t: str) ->bool:
+#         if len(s) != len(t):
+#             return False
+        
+#         countS, countT  = {}, {}
+        
+#         for i in range(len(s)):
+#             countS[s[i]] = 1 + countS.get(s[i], 0)
+#             countT[t[i]] = 1 + countT.get(t[i], 0)
+#         return countS == countT
+        
+        
+# print(isAnagram('racecar', 'carrace'))
+
+
+
+
+# solution2 
+
+def isAnagram(s: str, t: str) -> bool:
+    if len(s) != len(t):
+        return False
+
+    count = [0] * 26
+    for i in range(len(s)):
+        count[ord(s[i]) - ord('a')] += 1
+        count[ord(t[i]) - ord('a')] -= 1
+
+    for val in count:
+        if val != 0:
+            return False
+    return True
+
+print(isAnagram('car', 'rac'))
+print(isAnagram('racecar', 'carrace'))
