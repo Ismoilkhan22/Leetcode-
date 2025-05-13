f505e3e39fa93c5d0805d0743352acb6fd908992
Ismoilkhan22 <143698453+Ismoilkhan22@users.noreply.github.com>
2025-05-13 12:38:51 +0500

    removedublicates

--- leetcode_problems/day_one.py
+++ leetcode_problems/day_one.py
@@ -17,22 +17,38 @@ from typing import List
 # print(solution.twoSum(nums, target))
     
 
+# two sum problem
 
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#        n = len(nums)
+#        numMap = {}
+#        neg = 0
+#        for i in range(n):
+#            neg = target-nums[i]
+#            if neg in numMap:
+#                return [numMap[neg], i]
+#            numMap[nums[i]] = i
+           
+# nums = [3,2,4]
+# target = 6
+# solution = Solution()
+
+# print(solution.twoSum(nums, target))
+
+
+
+
+
+
+# 2.  remove dublicate from sorted array problems 26 from leetcode
 
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-       n = len(nums)
-       numMap = {}
-       neg = 0
-       for i in range(n):
-           neg = target-nums[i]
-           if neg in numMap:
-               return [numMap[neg], i]
-           numMap[nums[i]] = i
-           
-nums = [3,2,4]
-target = 6
-solution = Solution()
+    def removeDublicates(self, nums):
+        p = sorted(set(nums))
+        k = len(p)
 
-print(solution.twoSum(nums, target))
-    
+        for i in range(0,k):
+            nums[i]=list(p)[i]
+
+        return k
