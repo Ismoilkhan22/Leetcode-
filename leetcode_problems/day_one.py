cf87485b9e1b8acd06353b523536ce7b79137488
Ismoilkhan22 <143698453+Ismoilkhan22@users.noreply.github.com>
2025-05-13 12:34:21 +0500

    two sum nice solve

--- leetcode_problems/day_one.py
+++ leetcode_problems/day_one.py
@@ -1,20 +1,38 @@
 from typing import List
 
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+         
+#          for i in range(1,len(nums)):
+#             a = target-nums[i]
+#             if a in nums:
+#                 return [nums.index(i), nums.index(a)]
+#             else :
+#                 return None
+
+# nums = [3,2,4]
+# target = 6
+# solution = Solution()
+
+# print(solution.twoSum(nums, target))
+    
+
+
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-         
-         for i in range(1,len(nums)):
-            a = target-nums[i]
-            if a in nums:
-                return [nums.index(i), nums.index(a)]
-            else :
-                return None
-
+       n = len(nums)
+       numMap = {}
+       neg = 0
+       for i in range(n):
+           neg = target-nums[i]
+           if neg in numMap:
+               return [numMap[neg], i]
+           numMap[nums[i]] = i
+           
 nums = [3,2,4]
 target = 6
 solution = Solution()
 
 print(solution.twoSum(nums, target))
     
-
-
